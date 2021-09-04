bcc9da265d5efc3a3a2d3e78effa7f5fa5b25f58
dev-george <kgomulo@gmail.com>
2021-09-03 20:49:06 -0700

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-//import { CAMPSITES } from './shared/campsites';
+//import { CAMPSITES }  from '../shared/campsites';
 
 
 class CampsiteInfo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedCampsite: null
+        };
+    }
     renderComments(comments){
         if(comments){
             return (
@@ -11,16 +17,16 @@ class CampsiteInfo extends Component {
                 <div className = "col-md-5 m1">
                     <h4>Comments</h4>
                     {comments.map(
-                        comment => <div>
-                            {comment.text}
-                            {comment.author}
-                            {comment.date}
-                            {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                        comment => <div key={comment.id}>
+                            <p>{comment.text}</p>
+                            <p>{comment.author}</p>
+                            <p>{comment.date}</p>
+                            <p>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
 
                             </div>
 
                         
-                    )};
+                    )}
                 </div>
             )
             
@@ -32,6 +38,7 @@ class CampsiteInfo extends Component {
 
     }
     renderCampsite(campsite){
+        if (campsite) {
                 return (
                     <div className="col-md-5 m-1" >
 
@@ -45,16 +52,19 @@ class CampsiteInfo extends Component {
 
                     </div>
                 )
-
-
+        }
 
     }
     render() {
         if (this.props.campsite) {
-            return <div className="row" >
-                {this.renderCampsite(this.props.campsite)}
-                {this.renderComments(this.props.campsite.comments)}
-            </div>;
+            return (
+            <div className="container">
+                <div className="row" >
+                    {this.renderCampsite(this.props.campsite)}
+                    {this.renderComments(this.props.campsite.comments)}
+                </div>
+            </div>
+            )
         }
         return <div />
     }
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,19 +1,8 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-import CampsiteInfo from './CampsiteInfoComponent';
+
 
 class Directory extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedCampsite: null
-        };
-    }
-
-    onCampsiteSelect(campsite) {
-        //setState is for current data or object
-        this.setState({selectedCampsite: campsite});
-    }
 
 /*     renderSelectedCampsite(campsite) {
         if (campsite) {
@@ -34,7 +23,7 @@ class Directory extends Component {
         const directory = this.props.campsites.map(campsite => {
             return (
                 <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                    <Card onClick={() => this.props.onClick(campsite.id)}>
                         <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                         <CardImgOverlay>
                             <CardTitle>{campsite.name}</CardTitle>
@@ -49,9 +38,6 @@ class Directory extends Component {
                 <div className="row">
                     {directory}
                 </div>
-                <CampsiteInfo campsite = {this.state.selectedCampsite}>
-                    
-                </CampsiteInfo>
             </div>
         );
     }
