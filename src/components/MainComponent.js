a07a55e89b59c2c2955fb7da2fcb401e6d1d9127
dev-george <kgomulo@gmail.com>
2021-09-04 00:41:30 -0700

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,38 +2,43 @@ import React, { Component } from 'react';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Directory from './DirectoryComponent';
-import CampsiteInfo from './CampsiteInfoComponent';
+//import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES,
-            selectedCampsite: null
+            campsites: CAMPSITES
         };
     }
 
-    onCampsiteSelect(campsiteId) {
-        //setState is for current data or object
-        this.setState({selectedCampsite: campsiteId});
-    }
-
     render() {
+
+        const HomePage = () => {
+            return (
+                <Home />
+            );
+        };
+
         return (
             <div>
-                <Header dark color="primary">
-                    <div className="container">
-                        {/* <NavbarBrand href="/">NuCamp</NavbarBrand> */}
-                    </div>
-                </Header>
-                {/* Pass onClick handler arrow function as a prop from directory component */}
-                <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
-                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+                <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Redirect to='/home' />
+                </Switch>
                 <Footer />
             </div>
         );
-    };
+
+    }
+        
 }
+
 
 export default Main;
