1a2d1b44be9116b14f70dd52ccd72b389298b7ce
dev-george <kgomulo@gmail.com>
2021-09-18 00:04:25 -0700

    Combining Reducers

---
+++ src/redux/partners.js
@@ -0,0 +1,10 @@
+import { PARTNERS } from "../shared/partners";
+
+
+
+export const Partners = (state = PARTNERS, action) => {
+    switch (action.type){
+        default:
+            return state;
+    }
+};
